5ab5e9ff02f7144793acc5f2fbc30da69fe5d2d1
Jeff Wang <jeffwang4321@gmail.com>
2023-03-26 22:49:18 -0400

    Add loading screen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/1-Navbar";
 import HomeSection from "./components/2-HomeSection";
 import AboutSection from "./components/3-AboutSection";
@@ -7,11 +7,23 @@ import WorkSection from "./components/5-WorkSection";
 import ProjectSection from "./components/6-ProjectSection";
 import Footer from "./components/7-Footer";
 import ImageToggle from "./components/ImageToggle";
+import LoadingScreen from "./components/LoadingScreen";
 import { workImages } from "./components/0-Data";
 import { workSubtitles } from "./components/0-Data";
 
 function App() {
-  return (
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  return loading ? (
+    <LoadingScreen />
+  ) : (
     <>
       <Navbar />
       <HomeSection />
---
+++ src/components/LoadingScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/LoadingScreen.css";
+
+const LoadingScreen = () => {
+  return (
+    <div class="container">
+      <div class="loading">
+        <div class="bar"></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingScreen;
